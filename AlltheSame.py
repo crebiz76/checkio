4b5488ea51ae0cce16a165db30ae398fd53862a3
ToBeNew <47148092+crebiz76@users.noreply.github.com>
2019-11-06 23:19:17 +0900

    Delete comments

--- AlltheSame.py
+++ AlltheSame.py
@@ -4,9 +4,7 @@ In this mission you should check if all elements in the given list are equal.
 Input: List.
 
 Output: Bool.
-'''
 
-'''
 all_the_same([1, 1, 1]) == True
 all_the_same([1, 2, 1]) == False
 all_the_same(['a', 'a', 'a']) == True
@@ -28,64 +26,7 @@ def all_the_same(elements: List[Any]) -> bool:
         return 0
     else:
         return 1
-    
-    
-    '''    
-    #======================================    
-    a = False
-    b = False
-    c = False
-    d = False
-    i = 0
-    
-    for indata in elements:
-		i += 1
-		print(i, indata)
-        
-	
-    if(i <= 2):
-        d = True
-    else:
-        d = False
-        
 
-    if(indata == [] or i < 2):
-        d = True
-    else:
-        d = False
-
-        a = indata.pop()
-        b = indata.pop()
-        c = indata.pop()
-    
-    if((a == b) and (b==c)):
-        d = True
-    else:
-        d = False
-    
-    print(d)
-    return d
-
-    #======================================
-    d = input.count()
-    print(d)
-    if(input < 2):
-        c = True
-    else:
-        if(input[0] == input[1]):
-            a = True
-            if(input[1] == input[2]):
-                b = True
-    #======================================
-    if(input.index[0] != input.index[1]):
-        a = False
-    else:
-        if(input.index[1] != input.index[2]):
-            b = False
-    print(a&b&c)
-    return a & b & c
-    '''
-    
 if __name__ == '__main__':
     print("Example:")
     print(all_the_same([1, 1, 1]))
