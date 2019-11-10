314daa39e7cb46ce8a45201eb8b6ede42a26415b
ToBeNew <47148092+crebiz76@users.noreply.github.com>
2019-11-10 22:51:10 +0900

    Non Unique Elements complete

---
+++ NonUniqueElements.py
@@ -0,0 +1,66 @@
+'''
+If you have 50 different plug types, appliances wouldn't be available and would be very expensive. But once an electric outlet becomes standardized, many companies can design appliances, and competition ensues, creating variety and better prices for consumers.
+-- Bill Gates
+
+You are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique elements in this list. To do so you will need to remove all unique elements (elements which are contained in a given list only once). When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
+
+non-unique-elements
+
+Input: A list of integers.
+
+Output: An iterable of integers.
+
+Example:
+
+checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+checkio([1, 2, 3, 4, 5]) == []
+checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+1
+2
+3
+4
+
+How it is used: This mission will help you to understand how to manipulate arrays, something that is the basis for solving more complex tasks. The concept can be easily generalized for real world tasks. For example: if you need to clarify statistics by removing low frequency elements (noise).
+
+You can find out more about Python arrays in one of our articles featuring lists, tuples, array.array and numpy.array.
+
+Precondition:
+0 < len(data) < 1000
+'''
+
+#Your optional code here
+#You can import some modules or create additional functions
+
+def checkio(data):
+    #Your code here
+    #It's main function. Don't remove this function
+    #It's used for auto-testing and must return a result for check.  
+    
+    #replace this for solution
+    
+    #1. Input
+    #raw = data
+    import copy
+    raw = copy.deepcopy(data)
+    
+    #2. Processing
+    for i in raw:
+        j = raw.count(i)
+        if j == 1:
+            data.remove(i)
+    #3. Output
+    return data    
+
+#Some hints
+#You can use list.count(element) method for counting.
+#Create new list with non-unique elements
+#Loop over original list
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
