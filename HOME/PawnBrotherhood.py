fc584be04f030fe5317a200d24931c3a0affa579
ToBeNew <47148092+crebiz76@users.noreply.github.com>
2019-11-28 21:59:28 +0900

    Pawn Brotherhood complete

--- HOME/PawnBrotherhood.py
+++ HOME/PawnBrotherhood.py
@@ -1,5 +1,29 @@
 def safe_pawns(pawns: set) -> int:
-    return 0
+
+    pawn = list(pawns)
+    result = []
+    for i in pawn:
+        temp = []
+        x = ord(i[0])
+        y = int(i[1])
+        temp.append(x)
+        temp.append(y)
+        result.append(temp)
+    
+    cnt = 0; convlist = []; convset = {}
+    for j in result:
+        conv1 = []; conv2 = []
+        leftx1 = j[0] -1
+        leftx2 = j[0] + 1
+        lefty = j[1] - 1
+        conv1.append(leftx1)
+        conv1.append(lefty)
+        conv2.append(leftx2)
+        conv2.append(lefty)
+    
+        if (conv1 in result) or (conv2 in result): 
+            cnt += 1
+    return cnt
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
