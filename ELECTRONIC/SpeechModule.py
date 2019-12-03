fd1e10431eec1e8792ed1e455beea0210711a18b
ToBeNew <47148092+crebiz76@users.noreply.github.com>
2019-12-04 00:04:39 +0900

    Speech Module complete

--- ELECTRONIC/SpeechModule.py
+++ ELECTRONIC/SpeechModule.py
@@ -10,34 +10,47 @@ def checkio(number):
     temp = []; retval = ''
     if n >= 100:
         hund = n // 100
-        temp.append(hund)
         print('100>=...', hund)
         n -= (hund * 100)
+    else: 
+        hund = 0
+    temp.append(hund)
     if n >= 20:
         other_tens = n // 10
-        temp.append(other_tens)
         print('20>=...', other_tens)
-        n -= (other_tens * 10)
+        n -= (other_tens * 10)        
+    else:
+        other_tens = 0
+    temp.append(other_tens)
+
     if n >= 10:
         second_ten = n
-        temp.append(second_ten)
         print('10>=...', second_ten)
     else:
+        second_ten = 0
+    temp.append(second_ten)
+        
+    if n < 10:
         first_ten = n
-        temp.append(first_ten)
         print('10<...', first_ten)
+    else:
+        first_ten = 0
+    temp.append(first_ten)
     
     print(hund, other_tens, second_ten, first_ten)
     print(n, temp)
 
     # for i in range(len(temp)):
-    if hund > 0:        retval += FIRST_TEN[hund - 1] + ' ' + HUNDRED + ' '
-    if other_tens > 0:  retval += OTHER_TENS[other_tens - 2] + ' '
-    if second_ten > 0:  retval += SECOND_TEN[second_ten - 10] + ' '
+    if hund > 0:        retval += FIRST_TEN[hund - 1] + ' ' + HUNDRED
+    if temp[0] > 0 and (temp[1] > 0 or temp[2] > 0 or temp[3] > 0): retval += ' '
+    if other_tens > 0:  retval += OTHER_TENS[other_tens - 2]
+    if temp[1] > 0 and (temp[2] > 0 or temp[3] > 0): retval += ' '
+    if second_ten > 0:  retval += SECOND_TEN[second_ten - 10]
+    if temp[2] > 0 and temp[3] > 0:     retval += ' '
     if first_ten > 0:   retval += FIRST_TEN[first_ten - 1]
-            
+    # if last == 4:       retval += ' '
     # retval = str(FIRST_TEN[temp])
-    retval.strip()
+    # retval.strip(' ')
     print('Result=', retval)
     return retval
 
@@ -45,9 +58,9 @@ if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert checkio(4) == 'four', "1st example"
     assert checkio(133) == 'one hundred thirty three', "2nd example"
-    # assert checkio(12) == 'twelve', "3rd example"
+    assert checkio(12) == 'twelve', "3rd example"
     assert checkio(101) == 'one hundred one', "4th example"
-    # assert checkio(212) == 'two hundred twelve', "5th example"
-    # assert checkio(40) == 'forty', "6th example"
-    # assert not checkio(212).endswith(' '), "Don't forget strip whitespaces at the end of string"
+    assert checkio(212) == 'two hundred twelve', "5th example"
+    assert checkio(40) == 'forty', "6th example"
+    assert not checkio(212).endswith(' '), "Don't forget strip whitespaces at the end of string"
     print('Done! Go and Check it!')
