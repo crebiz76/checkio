da0d329e21f1914fd0c22a043a6cb6958e9e1677
ToBeNew <crebiz76@gmail.com>
2021-03-28 17:14:09 +0900

    Coversion from CamelCase complete

---
+++ python/SCIENTIFIC/ConversionFromCamelCase.py
@@ -0,0 +1,23 @@
+﻿def from_camel_case(name):
+    #replace this for solution
+    ret = ''
+    for i in name:
+        if i.isupper():
+            tmp = '_' + i.lower()
+        else:
+            tmp = i
+        ret += tmp
+    if(ret[0] == '_'):
+        ret = ret[1:]
+    return ret
+
+if __name__ == '__main__':
+    print("Example:")
+    print(from_camel_case("Name"))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert from_camel_case("MyFunctionName") == "my_function_name"
+    assert from_camel_case("IPhone") == "i_phone"
+    assert from_camel_case("ThisFunctionIsEmpty") == "this_function_is_empty"
+    assert from_camel_case("Name") == "name"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
--- python/SCIENTIFIC/HiddenWord.py
+++ python/SCIENTIFIC/HiddenWord.py
@@ -1,22 +1,26 @@
 def checkio(text, word):
     cut = text.replace(" ", "")
+    cut = cut.lower()
+    print("[Cut] =")
     print(cut)
+    print("[Count]",len(cut))
     
     result = []
     for j in range(len(word)):
         temp = []
         cnt = 0; row = 1; col = 0
+        print(word[j], "=====")
         for i in range(len(cut)):
             cnt += 1; col += 1
             if cut[i] == "\n":
                 col = 0; row += 1
             
             if word[j] == cut[i]:
-                # temp.append(j)
                 temp.append([row, col])
         result.append(temp)
     print(result)
 
+    
     return [2, 14, 2, 16]
     
 
